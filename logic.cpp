be1a1eb86373240af3723b1cc7d1fb81238af856
s4m1q <samir.sib2016@gmail.com>
2024-01-28 21:53:53 +0300

    Update logic.cpp
    добавил типы выбора калькулятора

--- logic.cpp
+++ logic.cpp
@@ -7,26 +7,94 @@
 #include "interface.h"
 #include "Header_alina+sani-fedoriko.h"
 #include "Count.h"
-
+#define Max_size 1000
 using namespace std;
 
 int main()
 {
     setlocale(LC_ALL, "RU");
-	string str = {};
 
-    
 
-	int type = interface_cal(); // выдаёт число, от него будет зависеть работа программы (выбор вида калькулятора)
 	
-		
+	
+	int type = interface_cal(); // выдаёт число, от него будет зависеть работа программы (выбор вида калькулятора)
 
-	string operation = {};
+
+	
+
+	
+
+
+	if (type == 1)
+	{
+		string str = {};
+		getline(cin, str);
+
+		std::stack<char> charStack;
+
+
+
+		for (char c : str)
+		{
+			charStack.push(c);
+		}
+
+	}
+	else if (type == 2)
+	{
+		string str = {};
+		getline(cin, str);
+
+		std::stack<char> charStack;
+
+
+
+		for (char c : str)
+		{
+			charStack.push(c);
+		}
+
+	}
+	else if (type == 3)
+	{
+		string str = {};
+		getline(cin, str);
+
+		std::stack<char> charStack;
+
+
+
+		for (char c : str)
+		{
+			charStack.push(c);
+		}
+
+	}
+	else if (type == 4)
+	{
+		string str = {};
+		getline(cin, str);
+
+		std::stack<char> charStack;
+
+
+
+		for (char c : str)
+		{
+			charStack.push(c);
+		}
+
+	}
+
+
+
+
+	/*string operation = {};
 	float a = 0, b = 0;
 	cin >> operation;
 	cin >> a >> b;
-	Calculate c(operation, a, b); // это вспомогательный калькулятор для обычных чисел. нужен такой же для мнимых (Саня Ф - это ты делаешь с классом итд)
-	double s = c.result();		// функции должны к нему обращаться для подсчёта, работает только с рациональынми числами.
+	Calculate c(operation, a, b);					// это вспомогательный калькулятор для обычных чисел. нужен такой же для мнимых (Саня Ф - это ты делаешь с классом итд)
+	double s = c.result();						// функции должны к нему обращаться для подсчёта, работает только с рациональынми числами.
 	cout << s;
-	
+	*/
 }
