4d1a2f32acce5d35cf438e4382313472bb32fd9c
i-am-a-saw <139990833+i-am-a-saw@users.noreply.github.com>
2024-01-30 16:47:32 +0300

    Добавлен класс комп. чисел с базовым набором функций

--- Header_alina+sani-fedoriko.h
+++ Header_alina+sani-fedoriko.h
@@ -1,1 +1,33 @@
+#pragma once
 
+/*
+Функции работают по следующему принципу: если нужно, например, прибавить к первому комп. числу второе, 
+то из первого числа вызывается функция "plus", в качестве аргументов передаются поля второго числа (real, imag). 
+Функция изменяет поля ПЕРВОГО числа (из которого она вызвана), второе при этом не меняется. 
+Это распростроняется на операции: + - * / sin cos tg ctg pow_comp log10 ln
+*/
+
+class Complex {
+public:
+	Complex() :real(0), imag(0) {}
+	Complex(double real, double imag);
+
+	void print()const;
+
+	void plus(double real, double imag);
+	void minus(double real, double imag);
+	void mult(double real, double imag);
+	void divide(double real, double imag);
+
+	void sinus();
+	void cosin();
+	void tg();
+	void ctg();
+
+	void pow_comp(double real, double imag); // возведение в комплексную или вещественную степень
+
+	void log_10();
+	void ln();
+private:
+	double real, imag;
+};
